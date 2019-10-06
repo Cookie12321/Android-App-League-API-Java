7ada6d813182221743d115755715dbc2c35bc44a
Albert Le <47728352+Cookie12321@users.noreply.github.com>
2019-10-05 18:17:20 -0700

    Add files via upload

--- testingClass.java
+++ testingClass.java
@@ -4,22 +4,30 @@ import java.util.Map;
 
 public class testingClass {
     public static void main(String[] args) throws IOException, JSONException {
+        //System.out.println("hello it works 1111");
 
-        riotAPI summonerSearch = new riotAPI("The Cookie", "na1");
+        riotAPI summonerSearch = new riotAPI("TheCookie", "na1");
 
-        System.out.println(summonerSearch.GetSummonerLevel());
-        System.out.println(summonerSearch.GetProfileIconID());
-        System.out.println(summonerSearch.GetSummonerName());
-        System.out.println(summonerSearch.GetAccountID());
-        System.out.println(summonerSearch.GetID());
+        //System.out.println("hello it works 2222");
 
-        System.out.println(summonerSearch.GetTotalGames());
+//        System.out.println(summonerSearch.GetSummonerLevel());
+//
+//        ///System.out.println("hello it works 3333");
+//
+//        System.out.println(summonerSearch.GetProfileIconID());
+//        System.out.println(summonerSearch.GetSummonerName());
+//        System.out.println(summonerSearch.GetAccountID());
+//        System.out.println(summonerSearch.GetID());
+//
+//        System.out.println(summonerSearch.GetTotalGames());
+//
+//        Map<CharSequence, Integer> championCounter = summonerSearch.GamesPerChampion();
+//
+//        // how to print out map
+//        for (CharSequence champion : championCounter.keySet()){
+//            System.out.println(champion + ": " + Integer.toString(championCounter.get(champion)));
+//        }
 
-        Map<CharSequence, Integer> championCounter = summonerSearch.GamesPerChampion();
-
-        // how to print out map
-        for (CharSequence champion : championCounter.keySet()){
-            System.out.println(champion + ": " + Integer.toString(championCounter.get(champion)));
-        }
+        summonerSearch.AvgGameLengthBy_Queue_Season(420, 13);
     }
 }
